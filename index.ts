1df216c61967e0a4f2b9af753c3c7255ed324f80
HUmnanANirudh <tanirudhganesh@gmail.com>
2025-12-15 21:23:35 +0530

    Implement basic Bun server and add start scripts in package.json

--- index.ts
+++ index.ts
@@ -1,1 +1,9 @@
-console.log("Hello via Bun!");
+const server = Bun.serve({
+  port: 3000,
+  fetch(res) {
+    const body = `Hello, Bun! You requested: ${res.url}\n`;
+    return new Response(body);
+  },
+});
+
+console.log(`Server running at http://localhost:${server.port}/`);
